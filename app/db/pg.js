70bc5eb816f65e9d271285001a0d83c800ff377a
Guillaume <jolibois.contact@gmail.com>
2023-08-16 23:47:16 +0200

    :tada: DB now hosted on elephantsql hoster

--- app/db/pg.js
+++ app/db/pg.js
@@ -4,11 +4,12 @@ import pg from 'pg';
 const debugSql = debug('app:sql');
 
 const pool = new pg.Pool({
-  host: 'localhost',
-  port: 5432,
-  database: 'lyrics_library',
-  user: 'admin',
-  password: 'admin',
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  // We can't use PGUSER because it's already used by Postgres
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
 })
 
 let queryCount = 0;
