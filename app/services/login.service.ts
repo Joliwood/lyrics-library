7b5019e2a06afaed888ffb35a8310944e620b32f
Guillaume Jolibois <jolibois.contact@gmail.com>
2024-01-21 11:42:48 +0100

    :hammer: Reformatted + full check script added

--- app/services/login.service.ts
+++ app/services/login.service.ts
@@ -2,10 +2,17 @@ import jwt from 'jsonwebtoken';
 
 export default {
   // Check if the user is authenticated through the token, check the expiration time and the IP
-  getUser(token, ip) {
+  getUser(token: string, ip: string) {
     if (!token) return null;
 
-    const user = jwt.verify(token, process.env.JWT_SECRET);
+    // TODO : Define types
+    let user: any;
+
+    if (process.env.JWT_SECRET) {
+      user = jwt.verify(token, process.env.JWT_SECRET);
+    }
+
+    if (!user) return null;
 
     if (user.ip !== ip) return null;
 
