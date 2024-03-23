3c6f0026ad973b1147f4cf5e6d01ac902a05a889
Guillaume Jolibois <jolibois.contact@gmail.com>
2024-03-23 17:44:53 +0100

    :tada: knex psql local option added

--- knexfile.ts
+++ knexfile.ts
@@ -7,6 +7,24 @@ const dirname = path.dirname(__filename);
 
 const knexConfig = {
 
+  local: {
+    client: 'pg',
+    connection: {
+      user: process.env.PG_USER,
+      database: process.env.PG_DATABASE,
+      password: process.env.PG_PASSWORD,
+      port: process.env.PG_PORT,
+      host: process.env.PG_HOST,
+      ssl: false,
+    },
+    migrations: {
+      directory: `${dirname}/db/migrations`,
+    },
+    seeds: {
+      directory: `${dirname}/db/seeds`,
+    },
+  },
+
   development: {
     client: 'pg',
     connection: {
@@ -16,50 +34,6 @@ const knexConfig = {
       port: process.env.PG_PORT,
       host: process.env.PG_HOST,
       ssl: true,
-    },
-    migrations: {
-      directory: `${dirname}/db/migrations`,
-    },
-    seeds: {
-      directory: `${dirname}/db/seeds`,
-    },
-  },
-
-  staging: {
-    client: 'pg',
-    connection: {
-      user: process.env.PG_USER,
-      database: process.env.PG_DATABASE,
-      password: process.env.PG_PASSWORD,
-      port: process.env.PG_PORT,
-      host: process.env.PG_HOST,
-      ssl: true,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: `${dirname}/db/migrations`,
-    },
-    seeds: {
-      directory: `${dirname}/db/seeds`,
-    },
-  },
-
-  production: {
-    client: 'pg',
-    connection: {
-      user: process.env.PG_USER,
-      database: process.env.PG_DATABASE,
-      password: process.env.PG_PASSWORD,
-      port: process.env.PG_PORT,
-      host: process.env.PG_HOST,
-      ssl: true,
-    },
-    pool: {
-      min: 2,
-      max: 10,
     },
     migrations: {
       directory: `${dirname}/db/migrations`,
