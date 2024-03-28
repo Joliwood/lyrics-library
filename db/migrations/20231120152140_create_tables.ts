543ec78f2234b598b2410718930882fbe4d0987e
Guillaume Jolibois <jolibois.contact@gmail.com>
2024-03-28 20:48:31 +0100

    :tada: songs now have release date field

--- db/migrations/20231120152140_create_tables.ts
+++ db/migrations/20231120152140_create_tables.ts
@@ -8,37 +8,38 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.dropTableIfExists('artist');
   await knex.schema.createTable('artist', (table) => {
     table.increments('id').primary();
-    table.text('name').notNullable();
-    table.text('picture');
     table.text('country');
-    table.text('password').notNullable();
     table.text('email').unique();
+    table.text('name').notNullable();
+    table.text('password').notNullable();
+    table.text('picture');
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
     table.timestamp('updated_at');
   });
   await knex.schema.createTable('album', (table) => {
     table.increments('id').primary();
-    table.text('title').notNullable();
+    table.integer('artist_id').notNullable().references('id').inTable('artist');
     table.integer('release_year');
     table.text('cover');
-    table.integer('artist_id').notNullable().references('id').inTable('artist');
+    table.text('title').notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
     table.timestamp('updated_at');
   });
   await knex.schema.createTable('song', (table) => {
     table.increments('id').primary();
-    table.text('title').notNullable();
+    table.integer('artist_id').notNullable().references('id').inTable('artist');
     table.integer('duration').notNullable();
+    table.integer('release_year');
     table.text('cover');
     table.text('lyrics');
-    table.integer('artist_id').notNullable().references('id').inTable('artist');
+    table.text('title').notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
     table.timestamp('updated_at');
   });
   await knex.schema.createTable('song_on_album', (table) => {
     table.integer('album_id').notNullable().references('id').inTable('album');
-    table.integer('song_id').notNullable().references('id').inTable('song');
     table.integer('position').notNullable();
+    table.integer('song_id').notNullable().references('id').inTable('song');
     table.primary(['album_id', 'song_id']);
   });
   await knex.schema.createTable('artist_like_song', (table) => {
